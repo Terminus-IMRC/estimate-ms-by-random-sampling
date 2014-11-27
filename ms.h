ac6fd6e371879e06be21d44d922012a4c18222d9
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-11-27 18:25:59 +0900

    add constant for printing a value of which type is MSTYPE by using printf

--- ms.h
+++ ms.h
@@ -2,9 +2,11 @@
 #define __MS_H_INCLUDED__
 
 #include <stdint.h>
+#include <inttypes.h>
 #include "X.h"
 
 	typedef int8_t MSTYPE;
+#define PRIMSTYPE PRIu8
 
 	void is_ms_init();
 	void is_ms_finalize();
